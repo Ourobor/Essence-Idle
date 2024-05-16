3787703f6f70a7aec6dc1ce95925124bfbc35226
Asches <asche@lowefrequency.com>
2024-05-16 14:08:08 -0400

    Implemented meditation button properly
    Meditation button now triggers a 10 tick long delay communicated by the new progress bar after which it gives the player 10 essence

--- app/src/main/java/com/ashe/essenceidle/CharacterState.kt
+++ app/src/main/java/com/ashe/essenceidle/CharacterState.kt
@@ -7,4 +7,17 @@ import androidx.compose.runtime.setValue
 
 class CharacterState {
     var essenceStrength by mutableStateOf(0)
+    val meditationTicks by mutableStateOf(10)
+    var meditationTicksLeft by mutableStateOf(0)
+
+    fun doTick(){
+        //Handle Meditation Tasks
+        if (meditationTicksLeft > 1){
+            meditationTicksLeft--;
+        }
+        else if (meditationTicksLeft == 1){
+            meditationTicksLeft = 0
+            essenceStrength += 10
+        }
+    }
 }
--- app/src/main/java/com/ashe/essenceidle/MainActivity.kt
+++ app/src/main/java/com/ashe/essenceidle/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ashe.essenceidle.ui.theme.EssenceIdleTheme
 import java.util.Timer
 import java.util.TimerTask
@@ -24,41 +31,77 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //TODO Verify that this is a good idea?
+        //Create the character state
+        /*TODO Verify that using this reference as well as the later mutableCharacterState
+           is good form */
         val charState = CharacterState()
 
+        //Create a repeating function to serve as each state "tick"
         val timer = Timer()
         val updateTask = object : TimerTask() {
             override fun run() {
-                charState.essenceStrength++
+                //Handle the tick
+                charState.doTick()
             }
         }
-        timer.schedule(updateTask, 0, 750)
+        timer.schedule(updateTask, 0, 1000)
 
         setContent {
+            //TODO: Update the theme. Manually setting it to always be darkmode is dumb.
             EssenceIdleTheme (darkTheme = true) {
-                // A surface container using the 'background' color from the theme
+                //Create a mutable reference to charState
+                val mutableCharState = remember { charState }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val mutableCharacterState = remember { charState }
-
-                    Column {
-                        Text(
-                            modifier = Modifier.fillMaxWidth()
-                                .padding(10.dp),
-                            color = MaterialTheme.colorScheme.primary,
-                            text = "Essence Strength: ${mutableCharacterState.essenceStrength}")
-                        Button(
-                            modifier = Modifier.padding(horizontal = 8.dp),
-                            onClick = { mutableCharacterState.essenceStrength++ }) {
-                            Text(text = "Meditate")
-                        }
-                    }
+                    MainElement(mutableCharState)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainElement(mutableCharState: CharacterState) {
+    Column {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 15.dp),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 20.sp,
+            text = "Essence Strength: ${mutableCharState.essenceStrength}")
+        MeditateButton(onClick = {
+            mutableCharState.meditationTicksLeft = mutableCharState.meditationTicks
+        },
+            ticksLeft = mutableCharState.meditationTicksLeft
+        )
+    }
+}
+
+@Composable
+fun MeditateButton(onClick:  () -> Unit, ticksLeft: Int = 0) {
+    Row() {
+        Button(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            onClick = onClick,
+            enabled = ticksLeft == 0
+        ) {
+            Text(text = "Meditate")
+        }
+
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(20.dp)
+                .align(Alignment.CenterVertically),
+            //TODO: Remove magic number
+            progress = (ticksLeft / 10.0).toFloat(),
+            strokeCap = StrokeCap.Round
+        )
     }
 }
 
@@ -66,5 +109,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     EssenceIdleTheme {
+        val characterState = CharacterState()
+        characterState.essenceStrength = 123
+        Column {
+            MainElement(mutableCharState = characterState)
+            MeditateButton(onClick = {}, ticksLeft = 3)
+        }
     }
 }
