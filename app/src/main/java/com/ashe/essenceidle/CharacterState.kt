0dfc79621cf368c26f9ea1caf06f95fe0d54bd67
Asches <asche@lowefrequency.com>
2024-05-15 21:15:52 -0400

    Started implementing timer stuff
    Also moved some logic into its own class to start splitting logic into its own place separate from ui code

---
+++ app/src/main/java/com/ashe/essenceidle/CharacterState.kt
@@ -0,0 +1,10 @@
+package com.ashe.essenceidle
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+
+class CharacterState {
+    var essenceStrength by mutableStateOf(0)
+}
--- app/src/main/java/com/ashe/essenceidle/MainActivity.kt
+++ app/src/main/java/com/ashe/essenceidle/MainActivity.kt
@@ -3,9 +3,7 @@ package com.ashe.essenceidle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,18 +12,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ashe.essenceidle.ui.theme.EssenceIdleTheme
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //TODO Verify that this is a good idea?
+        val charState = CharacterState()
+
+        val timer = Timer()
+        val updateTask = object : TimerTask() {
+            override fun run() {
+                charState.essenceStrength++
+            }
+        }
+        timer.schedule(updateTask, 0, 750)
+
         setContent {
             EssenceIdleTheme (darkTheme = true) {
                 // A surface container using the 'background' color from the theme
@@ -33,16 +42,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    var essenceStrength by remember { mutableStateOf(0) }
+                    val mutableCharacterState = remember { charState }
+
                     Column {
                         Text(
                             modifier = Modifier.fillMaxWidth()
                                 .padding(10.dp),
                             color = MaterialTheme.colorScheme.primary,
-                            text = "Essence Strength: $essenceStrength")
+                            text = "Essence Strength: ${mutableCharacterState.essenceStrength}")
                         Button(
                             modifier = Modifier.padding(horizontal = 8.dp),
-                            onClick = { essenceStrength++ }) {
+                            onClick = { mutableCharacterState.essenceStrength++ }) {
                             Text(text = "Meditate")
                         }
                     }
